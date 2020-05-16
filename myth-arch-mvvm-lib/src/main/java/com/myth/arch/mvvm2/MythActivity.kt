6efe3062e18197ea8c5069fd2ff356900f3d540d
yanweiqiang04496 <yanweiqiang04496@hellobike.com>
2020-05-16 17:37:35 +0800

    Refactor cancel BaseActivity and BaseFragment

--- myth-arch-mvvm-lib/src/main/java/com/myth/arch/mvvm2/MythActivity.kt
+++ myth-arch-mvvm-lib/src/main/java/com/myth/arch/mvvm2/MythActivity.kt
@@ -1,21 +1,9 @@
 package com.myth.arch.mvvm2
 
-import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.FragmentManager
-import androidx.lifecycle.LifecycleOwner
 
+/**
+ * Reserve class
+ */
 open class MythActivity : AppCompatActivity(), MythView {
-
-    override fun getContext2(): Context? {
-        return this
-    }
-
-    override fun getFragmentManager2(): FragmentManager? {
-        return supportFragmentManager
-    }
-
-    override fun getLifeCycleOwner(): LifecycleOwner {
-        return this
-    }
 }
--- myth-arch-mvvm-lib/src/main/java/com/myth/arch/mvvm2/MythFragment.kt
+++ myth-arch-mvvm-lib/src/main/java/com/myth/arch/mvvm2/MythFragment.kt
@@ -1,22 +1,9 @@
 package com.myth.arch.mvvm2
 
-import android.content.Context
-import android.os.Bundle
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
-import androidx.lifecycle.LifecycleOwner
 
+/**
+ * Reserve class
+ */
 class MythFragment : Fragment(), MythView {
-
-    override fun getContext2(): Context? {
-        return context
-    }
-
-    override fun getFragmentManager2(): FragmentManager? {
-        return fragmentManager
-    }
-
-    override fun getLifeCycleOwner(): LifecycleOwner {
-        return this
-    }
 }
