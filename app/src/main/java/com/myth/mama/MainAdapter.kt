40dadb568f9624fca72007ad6c9a61462b6cc9f1
yanweiqiang04496 <yanweiqiang04496@hellobike.com>
2020-06-30 19:33:36 +0800

    Add recyclerview for MainActivity

---
+++ app/src/main/java/com/myth/mama/MainAdapter.kt
@@ -0,0 +1,19 @@
+package com.myth.mama
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class MainAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+}
