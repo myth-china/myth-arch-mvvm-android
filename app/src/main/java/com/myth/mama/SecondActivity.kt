02724b0db27e8f9614745be26e84e5003f588bf1
yanweiqiang <yan66665@126.com>
2021-01-17 12:12:37 +0800

    Refactor UI

--- app/src/main/java/com/myth/mama/SecondActivity.kt
+++ app/src/main/java/com/myth/mama/SecondActivity.kt
@@ -1,8 +1,8 @@
 package com.myth.mama
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_second.*
 
 class SecondActivity : AppCompatActivity() {
